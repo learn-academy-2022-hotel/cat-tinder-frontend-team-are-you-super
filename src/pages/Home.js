642881dd4c6a0c9000d5034c9278063dd72ff258
monicaarganda <argandamonica1@gmail.com>
2023-02-06 12:14:48 -0800

    completed styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import superheroHome from "../assets/superheroHome.jpeg"
 
 const Home = () => {
   return (
     <>
-    <h1>Welcome to Are You Super, the exclusive dating app for SUPERHEROES.</h1>
+    <div className="home-title">
+    <h1 className="home-header">Welcome to <span className="home-span">Are You Super</span>, the exclusive dating app for <span className="home-span">SUPERHEROES</span>. If you're looking for <span className="home-span">SuperLove</span> this is the place for you! <span className="home-last-sentence">Find your match today!</span></h1>
+    <img className="home-image" alt="homeImage" src={superheroHome}/>
+    </div>
     </>
   );
 }
